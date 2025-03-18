9b3be315e8970791f2639177140493a1184c77d5
Tiran Wijerathne <tiranwijeratne@gmail.com>
2025-03-19 00:21:25 +0530

    Delete Keypair.tf

--- Keypair.tf
+++
@@ -1,8 +0,0 @@
-
-resource "aws_key_pair" "dove-key2" {
-  key_name   = "dove-key2"
-  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHxj0BOzXjOscqdGi8baauvxCOSE58crgZLtLIv4IoR8 ASUS@DESKTOP-45PG0NE"
-}
-
-
-
